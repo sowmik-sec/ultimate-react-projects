3265123af5806f06df35a83a645b48f95bfa6cd5
sowmik-sec <sowmik.sec@gmail.com>
2024-01-28 19:20:56 +0600

    add number selection

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -23,11 +23,14 @@ const Form = () => {
     <form className="add-form">
       <h3>What do you need for your trip?</h3>
       <select>
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
       <input type="text" placeholder="Item" />
+      <button>Add</button>
     </form>
   );
 };
