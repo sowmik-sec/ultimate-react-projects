17414bcdaad14f2dc1f30b8865f2b8236a66729b
sowmik-sec <sowmik.sec@gmail.com>
2024-01-29 17:42:50 +0600

    ask for confirmation to delete items

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -24,7 +24,10 @@ export default function App() {
   };
 
   const handleClearList = () => {
-    setItems("");
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
   };
 
   return (
