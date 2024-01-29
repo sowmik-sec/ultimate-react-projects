6903bcca272d19ebe2f56f94dc3685c544554c50
sowmik-sec <sowmik.sec@gmail.com>
2024-01-29 20:20:22 +0600

    ...

--- accordion-exercise/src/App.jsx
+++ accordion-exercise/src/App.jsx
@@ -46,7 +46,7 @@ function Accordion({ data }) {
 const AccordionItem = ({ num, title, curOpen, onOpen, children }) => {
   const isOpen = num === curOpen;
   const handleToggle = () => {
-    onOpen(num);
+    onOpen(isOpen ? null : num);
   };
   return (
     <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
