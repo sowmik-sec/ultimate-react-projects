8898f0e34555fbf214426760454d27fc082a1b37
sowmik-sec <sowmik.sec@gmail.com>
2024-01-29 22:19:33 +0600

    show bills only when it resides

--- pay-bill-calculator/src/components/PayBill/PayBill.jsx
+++ pay-bill-calculator/src/components/PayBill/PayBill.jsx
@@ -1,8 +1,12 @@
 export default function PayBill({ bill, myService, friendService }) {
   return (
-    <h1>
-      You pay ${bill + bill * ((myService + friendService) / 2 / 100)} ($
-      {bill} + ${bill * ((myService + friendService) / 2 / 100)})
-    </h1>
+    <>
+      {bill > 0 && (
+        <h1>
+          You pay ${bill + bill * ((myService + friendService) / 2 / 100)} ($
+          {bill} + ${bill * ((myService + friendService) / 2 / 100)})
+        </h1>
+      )}
+    </>
   );
 }
