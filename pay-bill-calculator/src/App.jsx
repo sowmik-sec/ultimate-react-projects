3030feefa8df5e3c0367b2a7f8230eab3c7f3e1d
sowmik-sec <sowmik.sec@gmail.com>
2024-01-29 21:54:37 +0600

    show all components in the ui

--- pay-bill-calculator/src/App.jsx
+++ pay-bill-calculator/src/App.jsx
@@ -1,14 +1,62 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Bill, PayBill, Reset, Service } from "./components";
+
+const options = [
+  {
+    id: 1,
+    title: "It was good(10%)",
+    value: 10,
+  },
+  {
+    id: 2,
+    title: "Very enjoyable(15%)",
+    value: 15,
+  },
+  {
+    id: 3,
+    title: "Absolutely amazing!(20%)",
+    value: 20,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [bill, setBill] = useState(0);
+  const [myService, setMyService] = useState(10);
+  const [friendService, setFriendService] = useState(10);
+
+  const handleMyServiceChange = (value) => {
+    setMyService(value);
+  };
+
+  const handleFriendServiceChange = (value) => {
+    setFriendService(value);
+  };
+
+  const handleReset = () => {
+    setBill(0);
+    setMyService(0);
+    setFriendService(0);
+  };
 
   return (
     <>
-      <h1>hello world</h1>
+      <Bill bill={bill} setBill={setBill} />
+      <Service
+        text={"How did you like the service?"}
+        options={options}
+        onServiceChange={handleMyServiceChange}
+      />
+      <Service
+        text={"How did your friend like the service?"}
+        options={options}
+        onServiceChange={handleFriendServiceChange}
+      />
+      <PayBill
+        bill={bill}
+        myService={myService}
+        friendService={friendService}
+      />
+      <Reset onReset={handleReset} />
     </>
   );
 }
