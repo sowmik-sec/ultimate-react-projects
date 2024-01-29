a410e5c07d213d22ec0df941b8c6c1f099c60646
sowmik-sec <sowmik.sec@gmail.com>
2024-01-29 21:54:14 +0600

    import and export components

---
+++ pay-bill-calculator/src/components/index.js
@@ -0,0 +1,6 @@
+import Bill from "./Bill/Bill";
+import Service from "./Service/Service";
+import PayBill from "./PayBill/PayBill";
+import Reset from "./Reset/Reset";
+
+export { Bill, Service, PayBill, Reset };
