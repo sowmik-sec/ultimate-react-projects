8c43d41b6c8add523a0017ad650d46f4bdad6287
sowmik-sec <sowmik.sec@gmail.com>
2024-01-29 17:15:46 +0600

    calculate statistics as derived state

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 // ];
 export default function App() {
   const [items, setItems] = useState([]);
+
   const handleAddItems = (item) => {
     setItems((items) => [...items, item]);
   };
@@ -31,7 +32,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -107,10 +108,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items.length)
+    return (
+      <p className="footer">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go."
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 };
