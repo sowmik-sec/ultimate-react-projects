65b0ab42beb70d913be7929b8992d09a3e033293
sowmik-sec <sowmik.sec@gmail.com>
2024-01-30 21:33:45 +0600

    import and export components

---
+++ eat-n-split/src/components/index.js
@@ -0,0 +1,7 @@
+import FormAddFriend from "./FormAddFriend/FormAddFriend";
+import FormSplitBill from "./FormSplitBill/FormSplitBill";
+import Friend from "./Friend/Friend";
+import FriendsList from "./FriendsList/FriendsList";
+import Button from "./Button/Button";
+
+export { FormAddFriend, FormSplitBill, Friend, FriendsList, Button };
