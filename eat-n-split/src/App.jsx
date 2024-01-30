b53583de4251cb0abc8a6b3d8ef2691440dcbc95
sowmik-sec <sowmik.sec@gmail.com>
2024-01-30 13:07:42 +0600

    setting project file structure

--- eat-n-split/src/App.jsx
+++ eat-n-split/src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <>
-      <h1>hello world</h1>
-    </>
-  );
-}
-
-export default App;
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
