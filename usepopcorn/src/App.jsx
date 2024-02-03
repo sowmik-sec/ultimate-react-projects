f7b9015807619c6b2cc7feb22321ab8f8b403439
sowmik-sec <sowmik.sec@gmail.com>
2024-02-03 10:43:15 +0600

    passing elements as props

--- usepopcorn/src/App.jsx
+++ usepopcorn/src/App.jsx
@@ -60,6 +60,15 @@ export default function App() {
         <NumResults movies={movies} />
       </Navbar>
       <Main>
+        {/* <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched} />
+            </>
+          }
+        /> */}
         <Box>
           <MovieList movies={movies} />
         </Box>
