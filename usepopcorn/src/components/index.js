ac84091f154bcf9f058dfc250a1c7de383012b3f
sowmik-sec <sowmik.sec@gmail.com>
2024-02-03 12:22:45 +0600

    export and import components

---
+++ usepopcorn/src/components/index.js
@@ -0,0 +1,27 @@
+import Box from "./Box/Box";
+import Logo from "./Logo/Logo";
+import Main from "./Main/Main";
+import Movie from "./Movie/Movie";
+import MovieList from "./MovieList/MovieList";
+import Navbar from "./Navbar/Navbar";
+import NumResults from "./NumResults/NumResults";
+import Search from "./Search/Search";
+import Star from "./Star/Star";
+import WatchedMovie from "./WatchedMovie/WatchedMovie";
+import WatchedMoviesList from "./WatchedMoviesList/WatchedMoviesList";
+import WatchedSummary from "./WatchedSummary/WatchedSummary";
+
+export {
+  Box,
+  Logo,
+  Main,
+  Movie,
+  MovieList,
+  Navbar,
+  NumResults,
+  Search,
+  Star,
+  WatchedMovie,
+  WatchedMoviesList,
+  WatchedSummary,
+};
