e8643023e58b05d91caa6e6c3d23b051e58c8c55
sowmik-sec <sowmik.sec@gmail.com>
2024-02-03 12:21:27 +0600

    create watch box component

---
+++ usepopcorn/src/components/WatchedBox/WatchedBox.jsx
@@ -0,0 +1,21 @@
+// export default  function WatchedBox() {
+//   const [isOpen2, setIsOpen2] = useState(true);
+//   const [watched, setWatched] = useState(tempWatchedData);
+
+//   return (
+//     <div className="box">
+//       <button
+//         className="btn-toggle"
+//         onClick={() => setIsOpen2((open) => !open)}
+//       >
+//         {isOpen2 ? "–" : "+"}
+//       </button>
+//       {isOpen2 && (
+//         <>
+//           <WatchedSummary watched={watched} />
+//           <WatchedMoviesList watched={watched} />
+//         </>
+//       )}
+//     </div>
+//   );
+// }
