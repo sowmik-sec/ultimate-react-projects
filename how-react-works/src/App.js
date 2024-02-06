7af13d5085edad8172c308d529c089a73721e8f8
sowmik-sec <sowmik.sec@gmail.com>
2024-02-06 18:32:52 +0600

    resetting state with the key prob

--- how-react-works/src/App.js
+++ how-react-works/src/App.js
@@ -26,7 +26,7 @@ export default function App() {
   );
 }
 
-console.log(<DifferentContent test={23} />);
+// console.log(<DifferentContent test={23} />);
 
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
@@ -41,7 +41,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
       ) : (
         <DifferentContent />
       )}
