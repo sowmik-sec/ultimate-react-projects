5fbf5df3b2223fe3c2039bbf9fdbac15e894706c
sowmik-sec <sowmik.sec@gmail.com>
2024-02-06 19:04:50 +0600

    handle undo

--- how-react-works/src/App.js
+++ how-react-works/src/App.js
@@ -66,9 +66,15 @@ function Tab({ num, activeTab, onClick }) {
 function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
-
+  console.log("RENDER");
   function handleInc() {
     setLikes(likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(likes);
   }
 
   return (
@@ -89,7 +95,7 @@ function TabContent({ item }) {
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
+        <button onClick={handleUndo}>Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>
