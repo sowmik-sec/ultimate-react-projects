cfefba23be99bf68a09e29622281d457a588f96d
sowmik-sec <sowmik.sec@gmail.com>
2024-02-09 21:38:26 +0600

    currency converter project

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,17 +1,40 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("EUR");
-
+  const [amount, setAmount] = useState(1);
+  const [result, setResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    async function convert() {
+      setIsLoading(true);
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+      setResult(data?.rates[toCurrency]);
+      setIsLoading(false);
+    }
+    if (fromCurrency === toCurrency) {
+      return setResult(amount);
+    }
+    convert();
+  }, [fromCurrency, toCurrency, amount]);
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
       <select
         value={fromCurrency}
         onChange={(e) => setFromCurrency(e.target.value)}
+        disabled={isLoading}
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
@@ -21,13 +44,16 @@ export default function App() {
       <select
         value={toCurrency}
         onChange={(e) => setToCurrency(e.target.value)}
+        disabled={isLoading}
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>
+        {result} {toCurrency}
+      </p>
     </div>
   );
 }
