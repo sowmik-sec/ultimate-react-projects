15540853aa0bb2fc8b3c89d8d1c457363309414a
sowmik-sec <sowmik.sec@gmail.com>
2024-02-09 10:46:04 +0600

    cleaning up the title

--- usepopcorn/src/components/MovieDetiles/MovieDetails.jsx
+++ usepopcorn/src/components/MovieDetiles/MovieDetails.jsx
@@ -56,6 +56,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   useEffect(() => {
     if (!title) return;
     document.title = `Movie | ${title}`;
+    return () => {
+      document.title = "usePopcorn";
+      console.log(`Clean up effect for movie ${title}`);
+    };
   }, [title]);
 
   return (
