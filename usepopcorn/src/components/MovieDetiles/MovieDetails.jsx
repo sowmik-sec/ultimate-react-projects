8430550bf77f54fe7f9b1130494fe441aed5d2b7
sowmik-sec <sowmik.sec@gmail.com>
2024-02-09 10:35:18 +0600

    add a new effect changing page title

--- usepopcorn/src/components/MovieDetiles/MovieDetails.jsx
+++ usepopcorn/src/components/MovieDetiles/MovieDetails.jsx
@@ -52,6 +52,12 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     }
     getMovieDetails();
   }, [selectedId]);
+
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+  }, [title]);
+
   return (
     <div className="details">
       {isLoading ? (
