8999bc33a0dd72d2fe2cc2465700f597a0a1aa72
sowmik-sec <sowmik.sec@gmail.com>
2024-02-09 11:41:28 +0600

    state set for 2 currencies

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,16 +1,27 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useState } from "react";
+
 export default function App() {
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+
   return (
     <div>
       <input type="text" />
-      <select>
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
