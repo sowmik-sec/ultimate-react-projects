c5b6f96683ccaf8d0b601a59862140a2eeb8958b
sowmik-sec <sowmik.sec@gmail.com>
2024-02-11 07:44:07 +0600

    Refs to select DOM elements

--- usepopcorn/src/components/Search/Search.jsx
+++ usepopcorn/src/components/Search/Search.jsx
@@ -5,10 +5,20 @@ import { useEffect, useRef } from "react";
 export default function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    console.log(inputEl.current);
-    inputEl.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
 
   // useEffect(function () {
   //   const el = document.querySelector(".search");
