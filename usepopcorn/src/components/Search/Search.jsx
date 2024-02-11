a460ca3f17fffa562da893afb3414e48af641e77
sowmik-sec <sowmik.sec@gmail.com>
2024-02-11 07:18:15 +0600

    how not to select DOM elements in React

--- usepopcorn/src/components/Search/Search.jsx
+++ usepopcorn/src/components/Search/Search.jsx
@@ -1,6 +1,12 @@
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+import { useEffect } from "react";
 
 export default function Search({ query, setQuery }) {
+  useEffect(function () {
+    const el = document.querySelector(".search");
+    console.log(el);
+    el.focus();
+  }, []);
   return (
     <input
       className="search"
