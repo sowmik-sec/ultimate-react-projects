d1a2a7f60d5226298f30ebfed84f53de89702216
sowmik-sec <sowmik.sec@gmail.com>
2024-02-12 09:11:08 +0600

    fixing header

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,5 +1,11 @@
+import { Header } from "./components";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
