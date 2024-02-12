efd47827a7335480ae470a1dd4023e44523fb66c
sowmik-sec <sowmik.sec@gmail.com>
2024-02-12 12:16:18 +0600

    displaying questions

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -12,6 +12,7 @@ const initialState = {
   questions: [],
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -38,7 +39,10 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -57,7 +61,7 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
--- react-quiz/src/components/Question/Question.jsx
+++ react-quiz/src/components/Question/Question.jsx
@@ -1,5 +1,14 @@
-function Question() {
-  return <div>Question</div>;
+import Options from "../Options/Options";
+
+/* eslint-disable react/prop-types */
+function Question({ question }) {
+  console.log(question);
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} />
+    </div>
+  );
 }
 
 export default Question;
