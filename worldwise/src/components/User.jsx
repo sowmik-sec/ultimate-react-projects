8920378debe66213ec1f402eec20a8735452947b
sowmik-sec <sowmik.sec@gmail.com>
2024-02-22 19:18:26 +0600

    implement login functionality and render user conditionally

--- worldwise/src/components/User.jsx
+++ worldwise/src/components/User.jsx
@@ -1,21 +1,28 @@
+import { useAuth } from "../hooks/useAuth";
 import styles from "./User.module.css";
+import { useNavigate } from "react-router-dom";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+// const FAKE_USER = {
+//   name: "Jack",
+//   email: "jack@example.com",
+//   password: "qwerty",
+//   avatar: "https://i.pravatar.cc/100?u=zz",
+// };
 
 function User() {
-  const user = FAKE_USER;
+  // const user = FAKE_USER;
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
-  function handleClick() {}
+  function handleClick() {
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
-      <img src={user.avatar} alt={user.name} />
-      <span>Welcome, {user.name}</span>
+      <img src={user?.avatar} alt={user?.name} />
+      <span>Welcome, {user?.name}</span>
       <button onClick={handleClick}>Logout</button>
     </div>
   );
