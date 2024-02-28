11e9377fe93b13fb31578c831a5eb81694ca14dd
sowmik-sec <sowmik.sec@gmail.com>
2024-02-28 11:34:23 +0600

    styling buttons element states and transitions

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -80,7 +80,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full  bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-slate-400 disabled:cursor-not-allowed"
+          >
             {isSubmitting ? "Placing order...." : "Order now"}
           </button>
         </div>
