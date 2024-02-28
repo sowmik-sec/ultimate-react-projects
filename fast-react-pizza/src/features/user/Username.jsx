23e6acc40c96d91b717825c8b7dbb6116076e717
sowmik-sec <sowmik.sec@gmail.com>
2024-02-28 11:02:53 +0600

    using flexbox

--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Habibi</div>;
+  return <div className="hidden text-sm font-semibold md:block">Habibi</div>;
 }
 
 export default Username;
