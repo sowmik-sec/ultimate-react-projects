69f777a87ef5c23e052c69abc92ccc8add90c086
sowmik-sec <sowmik.sec@gmail.com>
2024-02-28 10:19:06 +0600

    the box model spacing borders and display

--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Habibi</div>;
+  return <div className="hidden text-sm font-semibold">Habibi</div>;
 }
 
 export default Username;
