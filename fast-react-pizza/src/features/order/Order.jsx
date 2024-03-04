7a09a431492e093f85fae3bfae66f05d25203254
sowmik-sec <sowmik.sec@gmail.com>
2024-03-04 23:47:21 +0600

    update data without navigation

--- fast-react-pizza/src/features/order/Order.jsx
+++ fast-react-pizza/src/features/order/Order.jsx
@@ -10,6 +10,7 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -91,6 +92,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
