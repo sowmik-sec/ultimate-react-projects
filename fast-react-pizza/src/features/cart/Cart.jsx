e254fa07659df1fdafe6fba39919fb49ae22a0b6
sowmik-sec <sowmik.sec@gmail.com>
2024-03-04 19:11:52 +0600

    show empty cart while it is empty

--- fast-react-pizza/src/features/cart/Cart.jsx
+++ fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,39 +1,17 @@
 /* eslint-disable no-unused-vars */
-import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
+import EmptyCart from "./EmptyCart";
 import { useSelector, useDispatch } from "react-redux";
 import { clearCart, getCart } from "./cartSlice";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function Cart() {
   const dispatch = useDispatch();
   const username = useSelector((state) => state.user.username);
   const cart = useSelector(getCart);
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-3">
