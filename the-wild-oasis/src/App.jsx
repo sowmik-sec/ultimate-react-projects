ec4af53c1b41f2f1102cccfe0ae4180b1a7b6d1f
sowmik-sec <sowmik.sec@gmail.com>
2024-03-05 11:14:11 +0600

    styled component props and the css function

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -2,11 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -18,8 +14,10 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading type="h1">The Wild Oasis</Heading>
+        <Heading autoSave="h2">Check in and out</Heading>
         <Button onClick={() => alert("fuck me")}>Check in</Button>
+        <Heading as="h3">Form</Heading>
         <Button onClick={() => alert("fuck you")}>Check out</Button>
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
