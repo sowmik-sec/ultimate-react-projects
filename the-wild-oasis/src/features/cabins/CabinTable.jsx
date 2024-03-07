97895f95fc08d5e2e2385b5e2005d9c5ff62a52a
sowmik-sec <sowmik.sec@gmail.com>
2024-03-07 12:52:35 +0600

    use supabase db, create cabin table

--- the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabins.js";
+import Spinner from "../../ui/Spinner.jsx";
+import CabinRow from "./CabinRow.jsx";
 
+// eslint-disable-next-line no-unused-vars
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
 
@@ -9,6 +14,7 @@ const Table = styled.div`
   overflow: hidden;
 `;
 
+// eslint-disable-next-line no-unused-vars
 const TableHeader = styled.header`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -23,3 +29,35 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+  // eslint-disable-next-line no-unused-vars
+  const {
+    isLoading,
+    data: cabins,
+    // eslint-disable-next-line no-unused-vars
+    error,
+  } = useQuery({
+    queryKey: ["cabins"],
+    queryFn: getCabins,
+  });
+  console.log(cabins);
+  if (isLoading) return <Spinner />;
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins?.map((cabin) => (
+        <CabinRow key={cabin.id} cabin={cabin} />
+      ))}
+    </Table>
+  );
+}
+
+export default CabinTable;
