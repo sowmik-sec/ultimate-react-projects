96597a3a00f41ced39e79ce828f06e7a06a8df25
sowmik-sec <sowmik.sec@gmail.com>
2024-03-11 11:48:02 +0600

    ...

--- the-wild-oasis/src/services/apiSettings.js
+++ the-wild-oasis/src/services/apiSettings.js
@@ -13,11 +13,11 @@ export async function getSettings() {
 // We expect a newSetting object that looks like {setting: newValue}
 export async function updateSetting(newSetting) {
   const { data, error } = await supabase
-    .from("settings")
-    .update(newSetting)
-    // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
-    .eq("id", 1)
-    .single();
+      .from("settings")
+      .update(newSetting)
+      // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
+      .eq("id", 1)
+      .single();
 
   if (error) {
     console.error(error);
