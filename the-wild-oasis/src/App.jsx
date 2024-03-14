adbc3ef67420bb7ff47a06ed9e0beb2cc112537a
sowmik-sec <sowmik.sec@gmail.com>
2024-03-14 19:18:15 +0600

    define new route for Checkin component

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -13,6 +13,7 @@ import AppLayout from "./ui/AppLayout";
 import Login from "./pages/Login";
 import { Toaster } from "react-hot-toast";
 import Booking from "./pages/Booking";
+import Checkin from "./pages/Checkin";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -33,6 +34,7 @@ function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
             <Route path="bookings/:bookingId" element={<Booking />} />
+            <Route path="checkin/:bookingId" element={<Checkin />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
