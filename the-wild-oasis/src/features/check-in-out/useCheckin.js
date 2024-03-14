4b47d5ceac3ff0baa2063cc0d3a85a132a6477f7
sowmik-sec <sowmik.sec@gmail.com>
2024-03-14 19:50:49 +0600

    add optional breakfast

--- the-wild-oasis/src/features/check-in-out/useCheckin.js
+++ the-wild-oasis/src/features/check-in-out/useCheckin.js
@@ -3,14 +3,15 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateBooking } from "../../services/apiBookings";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-export function useChecking() {
+export function useCheckin() {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: checkin, isLoading: isCheckingIn } = useMutation({
-    mutationFn: (bookingId) =>
+    mutationFn: ({ bookingId, breakfast }) =>
       updateBooking(bookingId, {
         status: "checked-in",
         isPaid: true,
+        ...breakfast,
       }),
     onSuccess: (data) => {
       toast.success(`Booking #${data.id} successfully checked in`);
