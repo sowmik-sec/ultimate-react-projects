2445fb33feb87af1bcd85c4aefa3977f237a80f4
sowmik-sec <sowmik.sec@gmail.com>
2024-03-14 12:36:40 +0600

    APISide filtering bookings

--- the-wild-oasis/src/features/bookings/useBookings.js
+++ the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,15 +1,25 @@
 /* eslint-disable no-unused-vars */
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, error, bookings };
