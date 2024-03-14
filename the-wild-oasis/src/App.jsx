328448b1c0d554667b37890ba4adeb11f20a8ce9
sowmik-sec <sowmik.sec@gmail.com>
2024-03-14 17:02:24 +0600

    define new route

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -12,6 +12,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Login from "./pages/Login";
 import { Toaster } from "react-hot-toast";
+import Booking from "./pages/Booking";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -31,6 +32,7 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
