16ac3a70c076b9d02a198d7af1052f1254e85d68
sowmik-sec <sowmik.sec@gmail.com>
2024-03-14 21:39:05 +0600

    delete booking from show details page

--- the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -16,6 +16,9 @@ import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
 import { useCheckout } from "../check-in-out/useCheckout";
 import { HiArrowUpOnSquare } from "react-icons/hi2";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -29,6 +32,7 @@ function BookingDetail() {
   const { checkout, isCheckingOut } = useCheckout();
 
   const moveBack = useMoveBack();
+  const { isDeleting, deleteBooking } = useDeleteBooking();
 
   if (isLoading) return <Spinner />;
   const { status, id: bookingId } = booking;
@@ -66,6 +70,25 @@ function BookingDetail() {
             Chick Out
           </Button>
         )}
+
+        <Modal>
+          <Modal.Open opens="delete">
+            <Button variation="danger">Delete booking</Button>
+          </Modal.Open>
+
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="booking"
+              disabled={isDeleting}
+              onConfirm={() =>
+                deleteBooking(bookingId, {
+                  onSettled: () => navigate(-1),
+                })
+              }
+            />
+          </Modal.Window>
+        </Modal>
+
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
