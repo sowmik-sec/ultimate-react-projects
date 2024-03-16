1cae55f5406aaeaf6c10d2dc602e792ebabbbdc5
sowmik-sec <sowmik.sec@gmail.com>
2024-03-16 12:03:50 +0600

    create protected route

---
+++ the-wild-oasis/src/ui/ProtectedRoute.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  //1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  //2. if there is no authenticated user, redirect to the /login
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, isLoading, navigate]);
+
+  //3. while loading, show a spinner
+  if (isLoading) {
+    return (
+      <FullPage>
+        {" "}
+        <Spinner />
+      </FullPage>
+    );
+  }
+  //4. if there is a user, render the app
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
