bdaf717c94cfba438d6cc1b4cef96a8d1ad1ff2f
sowmik-sec <sowmik.sec@gmail.com>
2024-03-16 12:31:20 +0600

    create logout api and logout hook

--- the-wild-oasis/src/services/apiAuth.js
+++ the-wild-oasis/src/services/apiAuth.js
@@ -15,8 +15,13 @@ export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
   if (!session.session) return null;
   const { data, error } = await supabase.auth.getUser();
-  console.log(data);
+
   if (error) throw new Error(error.message);
   console.log(data);
   return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
