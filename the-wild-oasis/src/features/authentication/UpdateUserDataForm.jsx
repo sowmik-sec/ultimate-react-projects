c3805c262f2460bb015e3ec071339d7fc56fee86
sowmik-sec <sowmik.sec@gmail.com>
2024-03-18 14:25:43 +0600

    Complete UpdateUserDataForm component with email, full name, and avatar fields

--- the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
+++ the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 
 import Button from "../../ui/Button";
@@ -7,6 +8,7 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUser } from "./useUser";
+import { useUpdateUser } from "./useUpdateUser";
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
@@ -17,12 +19,29 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
+  const { updateUser, isUpdating } = useUpdateUser();
+
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser(
+      { fullName, avatar },
+      {
+        onSuccess: () => {
+          e.target.reset();
+          setAvatar(null);
+        },
+      },
+    );
   }
+
+  const handleCancel = () => {
+    setFullName(currentFullName);
+    setAvatar(null);
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -35,6 +54,7 @@ function UpdateUserDataForm() {
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
           id="fullName"
+          disabled={isUpdating}
         />
       </FormRow>
       <FormRow label="Avatar image">
@@ -42,13 +62,19 @@ function UpdateUserDataForm() {
           id="avatar"
           accept="image/*"
           onChange={(e) => setAvatar(e.target.files[0])}
+          disabled={isUpdating}
         />
       </FormRow>
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button
+          onClick={handleCancel}
+          type="reset"
+          variation="secondary"
+          disabled={isUpdating}
+        >
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdating}>Update account</Button>
       </FormRow>
     </Form>
   );
