cb92e8887123329a961a5505de73c5f0f255616a
sowmik-sec <sowmik.sec@gmail.com>
2024-03-19 19:33:38 +0600

    Update LoginForm and CabinRow components

--- the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("ahsan@habib.com");
-  const [password, setPassword] = useState("asdf123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLoading } = useLogin();
 
